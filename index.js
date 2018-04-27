1aa05321a64164170539340de60f3e9cd5a6ccac
Carlos Kazuo Mochizuki Ishizaka <bld_cishizaka@uolinc.com>
2018-04-27 18:34:44 -0300

    Adding package.json changes (add scripts)

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
-const { copy } = require('fs-extra')
+const { join } = require('path')
 const { spawn } = require('child_process')
+const { copy, writeFile } = require('fs-extra')
 const pkg = require('./package.json')
 
 const filter = name =>
   !/(?:(.git\/|node_modules|package|index.js|README.md))/i.test(name)
 
+const readLocalPackage = () =>
+  require(join(process.cwd(), 'package.json'))
+
+const updateScripts = localPkg => scripts =>
+  Object.assign({}, localPkg, { scripts: { ...localPkg.scripts, ...scripts } })
+
+const overridePackage = filename => data =>
+  writeFile(filename, JSON.stringify(data, null, 2))
+
 copy(__dirname, process.cwd(), { filter })
+  .catch(err => console.error(err))
   .then(() =>
     spawn('npm', ['init'], {
       cwd: process.cwd(),
@@ -14,6 +25,6 @@ copy(__dirname, process.cwd(), { filter })
       stdio: 'inherit'
     })
       .on('close', () =>
-        spawn('npm', ['install', '--save-dev'].concat(Object.keys(pkg.devDependencies))))
-  )
-  .catch(err => console.error(err))
+        spawn('npm', ['install', '--save-dev'].concat(Object.keys(pkg.devDependencies)))
+          .on('close', () =>
+            overridePackage('package.json')(updateScripts(readLocalPackage())({ lint: 'eslint .', precommit: 'npm run lint' })))))
