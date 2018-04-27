da7e2f20ae019800ac56160b0e35b3a51b579f60
Carlos Kazuo Mochizuki Ishizaka <bld_cishizaka@uolinc.com>
2018-04-27 18:19:18 -0300

    Added npm init & npm install

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 const { copy } = require('fs-extra')
+const { spawn } = require('child_process')
+const pkg = require('./package.json')
 
 const filter = name =>
-  !/(?:(.git|node_modules|package|index.js|README.md))/i.test(name)
+  !/(?:(.git\/|node_modules|package|index.js|README.md))/i.test(name)
 
 copy(__dirname, process.cwd(), { filter })
-  .then(() => console.log('success!'))
+  .then(() =>
+    spawn('npm', ['init'], {
+      cwd: process.cwd(),
+      detached: true,
+      stdio: 'inherit'
+    })
+      .on('close', () =>
+        spawn('npm', ['install', '--save-dev'].concat(Object.keys(pkg.devDependencies))))
+  )
   .catch(err => console.error(err))
